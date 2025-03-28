1eb3fb938a56351c342f5fa414b94ac02a226347
sanek2bas <sanek_2bas@mail.ru>
2025-03-28 22:53:54 +0300

    Coding_Algorithms + change .net to 9

---
+++ Coding_Algorithms/Processing.Strings.Efficiently/InvoiceItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Processing.Strings.Efficiently
+{
+    public sealed class InvoiceItem
+    {
+        public decimal Cost { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ Coding_Algorithms/Processing.Strings.Efficiently/Program.cs
@@ -0,0 +1,46 @@
+﻿using Processing.Strings.Efficiently;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var lineItems = GetInvoiceItems();
+        var string1 = DoStringConcatenation(lineItems);
+        Console.WriteLine(string1);
+        var string2 = DoStringBuilderConcatenation(lineItems);
+        Console.WriteLine(string2);
+    }
+
+    private static IList<InvoiceItem> GetInvoiceItems()
+    {
+        var items = new List<InvoiceItem>();
+        var rand = new Random();
+        for (int i = 0; i < 100; i++)
+        {
+            items.Add(
+                new InvoiceItem
+                {
+                    Cost = rand.Next(i),
+                    Description = "Invoice Item #" + (i + 1)
+                });
+        }
+        return items;
+    }
+
+    private static string DoStringConcatenation(IList<InvoiceItem> lineItems)
+    {
+        string report = "";
+        foreach (var item in lineItems)
+            report += $"{item.Cost:C} - {item.Description}\n";
+        return report;
+    }
+
+    private static string DoStringBuilderConcatenation(IList<InvoiceItem> lineItems)
+    {
+        var reportBuilder = new StringBuilder();
+        foreach (var item in lineItems)
+            reportBuilder.Append($"{item.Cost:C} - {item.Description}\n");
+        return reportBuilder.ToString();
+    }
+}
