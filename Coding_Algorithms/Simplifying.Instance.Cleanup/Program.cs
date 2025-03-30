18bd90a5b2ed07deae217a6ac977a93bb49a59e3
sanek2bas <sanek_2bas@mail.ru>
2025-03-31 00:20:13 +0300

    Simplifying.Instance.Cleanup part 1

---
+++ Coding_Algorithms/Simplifying.Instance.Cleanup/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private const string FileName = "Invoice.txt";
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
