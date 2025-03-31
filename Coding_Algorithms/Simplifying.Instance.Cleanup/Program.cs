58480afc66aee4fd1b4d34b9005cf9c4c7c6b57e
sanek2bas <sanek_2bas@mail.ru>
2025-03-31 11:25:57 +0300

    Simplifying.Instance.Cleanup

--- Coding_Algorithms/Simplifying.Instance.Cleanup/Program.cs
+++ Coding_Algorithms/Simplifying.Instance.Cleanup/Program.cs
@@ -1,9 +1,50 @@
 ﻿internal class Program
 {
-    private const string FileName = "Invoice.txt";
-
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var fileName = "Invoice.txt";
+        var baseDirectory = GetDataFolder(AppDomain.CurrentDomain.BaseDirectory,
+                                          AppDomain.CurrentDomain.FriendlyName);
+        var filePath = $"{baseDirectory}\\{fileName}";
+        Console.WriteLine(
+               "Invoice App\n" +
+               "-----------\n");
+        WriteDetails(filePath);
+        ReadDetails(filePath);
+    }
+
+    private static void WriteDetails(string filePath)
+    {
+        using var writer = new StreamWriter(filePath);
+        Console.WriteLine("Type details and press [Enter] to end.\n");
+        string detail;
+        do
+        {
+            Console.Write("Detail: ");
+            detail = Console.ReadLine();
+            writer.WriteLine(detail);
+        }
+        while (!string.IsNullOrWhiteSpace(detail));
+    }
+
+    private static void ReadDetails(string filePath)
+    {
+        Console.WriteLine("\nInvoice Details:\n");
+        using var reader = new StreamReader(filePath);
+        string detail;
+        do
+        {
+            detail = reader.ReadLine();
+            Console.WriteLine(detail);
+        }
+        while (!string.IsNullOrWhiteSpace(detail));
+    }
+
+    private static string GetDataFolder(string startDirectory, string targetFolder)
+    {
+        var directory = Directory.GetParent(startDirectory);
+        while (directory.Name != targetFolder)
+            directory = directory.Parent;
+        return directory.FullName;
     }
 }
