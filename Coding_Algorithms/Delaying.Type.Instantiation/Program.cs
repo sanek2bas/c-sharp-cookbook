52e19ab7fb82b8ccf516d22d02a14ea0a2bf3198
sanek2bas <sanek_2bas@mail.ru>
2025-03-31 14:51:05 +0300

    Delaying.Type.Instantiation

--- Coding_Algorithms/Delaying.Type.Instantiation/Program.cs
+++ Coding_Algorithms/Delaying.Type.Instantiation/Program.cs
@@ -1,12 +1,45 @@
-﻿internal class Program
+﻿using Microsoft.Extensions.DependencyInjection;
+
+internal class Program
 {
+    public static ServiceProvider Container;
+
+    readonly Lazy<InvoiceRepository> InvoiceRep =
+        new Lazy<InvoiceRepository>();
+
+    readonly Lazy<IInvoiceRepository> InvoiceRepFactory =
+        new Lazy<IInvoiceRepository>(CreateInvoiceRepositoryInstance);
+
+    readonly Lazy<IInvoiceRepository> InvoiceRepIoC =
+        new Lazy<IInvoiceRepository>(CreateInvoiceRepositoryFromIoC);
+
+    private static IInvoiceRepository CreateInvoiceRepositoryInstance()
+    {
+        return new InvoiceRepository();
+    }
+
+    private static IInvoiceRepository CreateInvoiceRepositoryFromIoC()
+    {
+        return Container.GetRequiredService<IInvoiceRepository>();
+    }
+
     static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        Container = new ServiceCollection().
+                        AddTransient<IInvoiceRepository, InvoiceRepository>().
+                        BuildServiceProvider();
+        new Program().Run();
     }
 
     void Run()
     {
-        
+        IInvoiceRepository viaLazyDefault = InvoiceRep.Value;
+        viaLazyDefault.AddInvoiceCategory("Via Lazy Default \n");
+
+        IInvoiceRepository viaLazyFactory = InvoiceRepFactory.Value;
+        viaLazyFactory.AddInvoiceCategory("Via Lazy Factory \n");
+
+        IInvoiceRepository viaLazyIoC = InvoiceRepIoC.Value;
+        viaLazyIoC.AddInvoiceCategory("Via Lazy IoC \n");
     }
 }
