00eeb31e5095c540ff7fb8eb0f3fa96b6c8a4011
sanek2bas <sanek_2bas@mail.ru>
2025-03-31 10:07:34 +0300

    added stopwatch

--- Coding_Algorithms/Processing.Strings.Efficiently/Program.cs
+++ Coding_Algorithms/Processing.Strings.Efficiently/Program.cs
@@ -1,15 +1,27 @@
 ﻿using Processing.Strings.Efficiently;
+using System.Diagnostics;
 using System.Text;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
+        var sw = new Stopwatch();
         var lineItems = GetInvoiceItems();
+
+        sw.Start();
         var string1 = DoStringConcatenation(lineItems);
         Console.WriteLine(string1);
+        sw.Stop();
+        Console.WriteLine($"{nameof(DoStringConcatenation)} time: {sw.ElapsedMilliseconds} ms");
+
+        Console.WriteLine();
+
+        sw.Restart();
         var string2 = DoStringBuilderConcatenation(lineItems);
         Console.WriteLine(string2);
+        sw.Stop();
+        Console.WriteLine($"{nameof(DoStringBuilderConcatenation)} time: {sw.ElapsedMilliseconds} ms");
     }
 
     private static IList<InvoiceItem> GetInvoiceItems()
