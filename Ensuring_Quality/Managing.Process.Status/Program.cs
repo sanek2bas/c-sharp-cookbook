15d7ac4f100a247148571762ef61eee0592fee8a
sanek2bas <sanek_2bas@mail.ru>
2025-04-01 11:19:18 +0300

    Managing.Process.Status

---
+++ Ensuring_Quality/Managing.Process.Status/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Processing Orders Started");
+            ProcessOrders();
+        }
+        catch (ArgumentException ae)
+        {
+            Console.WriteLine('\n' + ae.ToString() + '\n');
+        }
+        finally
+        {
+            Console.WriteLine("Processing Orders Complete");
+        }
+    }
+
+    private static void ProcessOrders()
+    {
+        throw new ArgumentException();
+    }
+}
