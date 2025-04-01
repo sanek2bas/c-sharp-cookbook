5be94bc106d26c429620393cb38c6db60ecbfa83
sanek2bas <sanek_2bas@mail.ru>
2025-04-01 12:14:41 +0300

    Transforming.Object.Shape

---
+++ Querying_With_LINQ/Transforming.Object.Shape/Program.cs
@@ -0,0 +1,19 @@
+﻿using Transforming.Object.Shape;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var context = new InMemoryContext();
+
+        var salesPersonLookup =
+            (from person in context.SalesPeople
+             select (person.ID, person.Name))
+            .ToList();
+
+        Console.WriteLine("Sales People\n");
+
+        salesPersonLookup.ForEach(person => 
+            Console.WriteLine($"{person.ID}. {person.Name}"));
+    }
+}
