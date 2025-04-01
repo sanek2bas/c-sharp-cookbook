4a0f566568a7d30ca19dbe14062a729b8303afe1
sanek2bas <sanek_2bas@mail.ru>
2025-04-01 23:32:16 +0300

    Joining.Data

---
+++ Querying_With_LINQ/Joining.Data/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Joining.Data
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public decimal Price { get; set; }
+        public string Region { get; set; }
+    }
+}
---
+++ Querying_With_LINQ/Joining.Data/Program.cs
@@ -0,0 +1,34 @@
+﻿using Joining.Data;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var context = new InMemoryContext();
+
+        var salesProducts = 
+            (from product in context.Products
+             join person in context.SalesPeople on
+             (product.Region, product.Type)
+             equals
+             (person.Region, person.ProductType)
+             into prodPersonTemp
+             from prodPerson in prodPersonTemp.DefaultIfEmpty()
+             select new
+             {
+                 Person = prodPerson?.Name ?? "(none)",
+                 Product = product.Name,
+                 product.Region,
+                 product.Type
+             })
+        .ToList();
+
+        Console.WriteLine("Sales People\n");
+        salesProducts.ForEach(salesProd => 
+            Console.WriteLine(
+                $"Person: {salesProd.Person}\n" +
+                $"Product: {salesProd.Product}\n" +
+                $"Region: {salesProd.Region}\n" +
+                $"Type: {salesProd.Type}\n"));
+    }
+}
