a1ca5e3ae9c5cbeb1262e81567198408a2a4a4a1
sanek2bas <sanek_2bas@mail.ru>
2025-04-01 23:48:29 +0300

    Grouping.Data

---
+++ Querying_With_LINQ/Grouping.Data/Program.cs
@@ -0,0 +1,24 @@
+﻿using Grouping.Data;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var context = new InMemoryContext();
+
+        var salesPeopleByRegion =
+            (from person in context.SalesPeople
+             group person by person.Region
+             into personGroup
+             select personGroup)
+             .ToList();
+
+        Console.WriteLine("Sales People by Region");
+        foreach (var region in salesPeopleByRegion)
+        {
+            Console.WriteLine($"\nRegion: {region.Key}");
+            foreach (var person in region)
+                Console.WriteLine($" {person.Name}");
+        }
+    }
+}
