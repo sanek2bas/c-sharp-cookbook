e1150635539b78229559a76abbdf07e2b8f1f79d
sanek2bas <sanek_2bas@mail.ru>
2025-04-01 10:28:02 +0300

    Customizing.Class.String.Representation

---
+++ Ensuring_Quality/Customizing.Class.String.Representation/Program.cs
@@ -0,0 +1,16 @@
+﻿using Customizing.Class.String.Representation;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var order = new Order
+        {
+            ID = 7,
+            CustomerName = "Acme",
+            Created = DateTime.Now,
+            Amount = 2_718_281.83m
+        };
+        Console.WriteLine(order);
+    }
+}
