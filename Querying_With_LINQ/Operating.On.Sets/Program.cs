fffbe73cbdc099907dc91e09ff638c8cb62bbb2e
sanek2bas <sanek_2bas@mail.ru>
2025-04-02 22:21:08 +0300

    Operating.On.Sets + refactoring

---
+++ Querying_With_LINQ/Operating.On.Sets/Program.cs
@@ -0,0 +1,89 @@
+﻿using Operating.On.Sets;
+
+internal class Program
+{
+    static InMemoryContext ctx = new InMemoryContext();
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("\nLINQ Set Operations");
+
+        DoUnion();
+        DoExcept();
+        DoIntersection();
+
+        Console.WriteLine("\nComplete.\n");
+    }
+
+    private static void DoUnion()
+    {
+        var dataSource1 =
+            (from person in ctx.SalesPeople
+             where person.ID < 3
+             select person)
+             .ToList();
+        
+        var dataSource2 =
+            (from person in ctx.SalesPeople
+             where person.ID > 2
+             select person)
+             .ToList();
+        
+        List<SalesPerson> union =
+            dataSource1.
+            Union(dataSource2, new SalesPerson()).
+            ToList();
+
+        PrintResults(union, "Union Results");
+    }
+
+    private static void DoExcept()
+    {
+        var dataSource1 =
+            (from person in ctx.SalesPeople
+             select person)
+             .ToList();
+        
+        var dataSource2 =
+            (from person in ctx.SalesPeople
+             where person.ID == 4
+             select person)
+             .ToList();
+
+        List<SalesPerson> union =
+            dataSource1.
+            Except(dataSource2, new SalesPerson()).
+            ToList();
+        
+        PrintResults(union, "Except Results");
+    }
+
+    private static void DoIntersection()
+    {
+        var dataSource1 =
+            (from person in ctx.SalesPeople
+             where person.ID < 4
+             select person)
+             .ToList();
+        
+        var dataSource2 =
+            (from person in ctx.SalesPeople
+             where person.ID > 2
+             select person)
+             .ToList();
+        
+        List<SalesPerson> union =
+            dataSource1.
+            Intersect(dataSource2, new SalesPerson()).
+            ToList();
+        
+        PrintResults(union, "Intersect Results");
+    }
+
+    static void PrintResults(List<SalesPerson> salesPeople, string title)
+    {
+        Console.WriteLine($"\n{title}\n");
+        salesPeople.ForEach(person =>
+            Console.WriteLine($"{person.ID}. {person.Name}"));
+    }
+}
---
+++ Querying_With_LINQ/Operating.On.Sets/SalesPerson.cs
@@ -0,0 +1,23 @@
+﻿namespace Operating.On.Sets
+{
+    public class SalesPerson : IEqualityComparer<SalesPerson>
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string Region { get; set; }
+        public string ProductType { get; set; }
+
+        public bool Equals(SalesPerson x, SalesPerson y)
+        {
+            return x.ID == y.ID;
+        }
+
+        public int GetHashCode(SalesPerson obj)
+        {
+            return ID.GetHashCode();
+        }
+    }
+}
