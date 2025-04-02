a9e173fb8328020d6e5d6e1355ccd793dc290d14
sanek2bas <sanek_2bas@mail.ru>
2025-04-02 11:01:29 +0300

    Querying.Distinct.Objects part 1

---
+++ Querying_With_LINQ/Querying.Distinct.Objects/Program.cs
@@ -0,0 +1,32 @@
+﻿using Querying.Distinct.Objects;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var context = new InMemoryContext();
+
+        var salesPeopleWithoutComparer =
+            (from person in context.SalesPeople
+             select person)
+             .Distinct()
+             .ToList();
+
+        PrintResults(salesPeopleWithoutComparer, "Without Comparer");
+
+        var salesPeopleWithComparer =
+            (from person in context.SalesPeople
+             select person)
+             .Distinct(new SalesPersonComparer())
+             .ToList();
+
+        PrintResults(salesPeopleWithComparer, "With Comparer");
+    }
+
+    private static void PrintResults(List<SalesPerson> salesPeople, string title)
+    {
+        Console.WriteLine($"\n{title}\n");
+        salesPeople.ForEach(person =>
+            Console.WriteLine($"{person.ID}. {person.Name}"));
+    }
+}
---
+++ Querying_With_LINQ/Querying.Distinct.Objects/SalesPersonComparer.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Querying.Distinct.Objects
+{
+    public class SalesPersonComparer : IEqualityComparer<SalesPerson>
+    {
+        public bool Equals(SalesPerson? x, SalesPerson? y)
+        {
+            return x?.ID == y?.ID;
+        }
+
+        public int GetHashCode([DisallowNull] SalesPerson obj)
+        {
+            return obj == null ? 0 : obj.GetHashCode();
+        }
+    }
+}
