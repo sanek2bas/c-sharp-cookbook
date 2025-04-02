cfa1ad5637049b13843aafb09f248142a6c93c0f
sanek2bas <sanek_2bas@mail.ru>
2025-04-02 22:52:38 +0300

    Querying.In.Parallel

---
+++ Querying_With_LINQ/Querying.In.Parallel/Program.cs
@@ -0,0 +1,24 @@
+﻿using Querying.In.Parallel;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<SalesPerson> salesPeople = new InMemoryContext().SalesPeople;
+        var result =
+            (from person in salesPeople.AsParallel()
+             select ProcessPerson(person))
+             .ToList();
+    }
+
+    private static SalesPerson ProcessPerson(SalesPerson person)
+    {
+        Console.WriteLine(
+            $"Starting sales person #{person.ID}. {person.Name}");
+        // complex in-memory processing
+        Thread.Sleep(500);
+        Console.WriteLine(
+        $"Completed sales person #{person.ID}. {person.Name}");
+        return person;
+    }
+}
