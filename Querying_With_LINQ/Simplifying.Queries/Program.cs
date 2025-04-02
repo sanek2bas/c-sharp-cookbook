a8d6a5b33976383ab9eaa6e564998733b1484afd
Alexander Gordeev <a.gordeev@trbonet.com>
2025-04-02 12:54:42 +0300

    Simplifying.Queries

---
+++ Querying_With_LINQ/Simplifying.Queries/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        decimal TotalSales = 0;
+
+        var salesPeopleWithAddresses =
+            (from person in new InMemoryContext().SalesPeople
+             let FullAddress =
+             $"{person.Address}\n" +
+             $"{person.City}, {person.PostalCode}"
+             let salesOkay =
+                decimal.TryParse(person.TotalSales, out TotalSales)
+                select new
+                {
+                person.ID,
+                person.Name,
+                FullAddress,
+                TotalSales
+                })
+            .ToList();
+
+            Console.WriteLine($"\nSales People and Addresses\n");
+            
+            salesPeopleWithAddresses.ForEach(person =>
+                Console.WriteLine(
+            $"{person.ID}. {person.Name}: {person.TotalSales:C}\n" +
+            $"{person.FullAddress}\n"));
+    }
+}
