f11cc627df345b5d36149a9b4eb0209a02bd3316
sanek2bas <sanek_2bas@mail.ru>
2025-04-03 00:00:48 +0300

    Simplifying.Immutable.Type.Assignments

---
+++ C_SharpLanguage_Highlights/Simplifying.Immutable.Type.Assignments/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private record Address(string Street, string City, string State, string Zip);
+
+    private static void Main(string[] args)
+    {
+        Address addressPre = new(
+            Street: "567 8th Ave.",
+            City: "Some Place",
+            State: "YY",
+            Zip: "12345-7890");
+        Address addressPost =
+            addressPre with
+            {
+                Street = "569 8th Ave."
+            };
+        Console.WriteLine($"Pre: {addressPre}");
+        Console.WriteLine($"Post: {addressPost}");
+        Console.WriteLine(
+            $"Value Equal: " +
+            $"{addressPre == addressPost}");
+    }
+}
