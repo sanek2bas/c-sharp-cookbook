16c98a1eaee3c224cb9f307f5589a3a51d78beed
sanek2bas <sanek_2bas@mail.ru>
2025-04-02 23:57:36 +0300

    Creating.Immutable.Types

---
+++ C_SharpLanguage_Highlights/Creating.Immutable.Types/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private record Address(string Street, string City, string State, string Zip);
+
+    private static void Main(string[] args)
+    {
+        var addressClassic = new Address(
+            Street: "567 8th Ave.",
+            City: "Some Place",
+            State: "YY",
+            Zip: "12345-7890");
+
+        Address addressCtorInit = new(
+            Street: "567 8th Ave.",
+            City: "Some Place",
+            State: "YY",
+            Zip: "12345-7890");
+
+        Console.WriteLine(
+            $"Value Equal: " +
+            $"{addressClassic == addressCtorInit}");
+        Console.WriteLine(
+            $"Reference Equal: " +
+            $"{ReferenceEquals(addressClassic, addressCtorInit)}");
+        Console.WriteLine(
+            $"{nameof(addressClassic)}: {addressClassic}");
+        Console.WriteLine(
+            $"{nameof(Address)}: {addressCtorInit}");
+    }
+}
