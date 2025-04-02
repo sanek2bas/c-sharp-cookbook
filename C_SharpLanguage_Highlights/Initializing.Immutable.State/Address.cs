3bdc00eb9108a287fbc4ed6232888e51c641db50
sanek2bas <sanek_2bas@mail.ru>
2025-04-02 23:34:47 +0300

    Initializing.Immutable.State

---
+++ C_SharpLanguage_Highlights/Initializing.Immutable.State/Address.cs
@@ -0,0 +1,24 @@
+﻿namespace Initializing.Immutable.State
+{
+    public class Address
+    {
+        public string Street { get; init; }
+        public string City { get; init; }
+        public string State { get; init; }
+        public string Zip { get; init; }
+
+        public Address() { }
+
+        public Address(
+            string street,
+            string city,
+            string state,
+            string zip)
+        {
+            Street = street;
+            City = city;
+            State = state;
+            Zip = zip;
+        }
+    }
+}
---
+++ C_SharpLanguage_Highlights/Initializing.Immutable.State/Program.cs
@@ -0,0 +1,21 @@
+﻿using Initializing.Immutable.State;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Address addressObjectInit = new()
+        {
+            Street = "123 4th St.",
+            City = "My City",
+            State = "ZZ",
+            Zip = "55555-3333"
+        };
+
+        Address addressCtorInit = new(
+            street: "567 8th Ave.",
+            city: "Some Place",
+            state: "YY",
+            zip: "12345-7890");
+    }
+}
