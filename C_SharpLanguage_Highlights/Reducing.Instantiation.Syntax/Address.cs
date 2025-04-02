1d6b05c8051bf7ac3710ec9eb54f163f4ef8fa28
sanek2bas <sanek_2bas@mail.ru>
2025-04-02 23:32:05 +0300

    Reducing.Instantiation.Syntax

---
+++ C_SharpLanguage_Highlights/Reducing.Instantiation.Syntax/Address.cs
@@ -0,0 +1,24 @@
+﻿namespace Reducing.Instantiation.Syntax
+{
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+
+        public Address() { }
+
+        public Address(
+            string street,
+            string city,
+            string state,
+            string zip)
+        {
+            Street = street;
+            City = city;
+            State = state;
+            Zip = zip;
+        }
+    }
+}
---
+++ C_SharpLanguage_Highlights/Reducing.Instantiation.Syntax/Program.cs
@@ -0,0 +1,26 @@
+﻿using Reducing.Instantiation.Syntax;
+
+internal class Program
+{
+    Address addressOld = new Address();
+    Address addressNew = new();
+
+    private static void Main(string[] args)
+    {
+        var addressLocalVar = new Address();
+        Address addressLocalOld = new Address();
+        Address addressLocalNew = new();
+        Address addressObjectInit = new()
+        {
+            Street = "123 4th St.",
+            City = "My City",
+            State = "ZZ",
+            Zip = "55555-3333"
+        };
+        Address addressCtorInit = new(
+            street: "567 8th Ave.",
+            city: "Some Place",
+            state: "YY",
+            zip: "12345-7890");
+    }
+}
