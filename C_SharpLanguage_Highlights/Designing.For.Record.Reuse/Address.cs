b427eeb49821202e52df298dabe8987655002e36
TRBONET\a.gordeev <a.gordeev@trbonet.com>
2025-04-03 10:09:53 +0300

    Designing.For.Record.Reuse

---
+++ C_SharpLanguage_Highlights/Designing.For.Record.Reuse/Address.cs
@@ -0,0 +1,20 @@
+public abstract record AddressBase(
+    string Street, string City, string State, string Zip);
+
+public record MailingAddress(
+    string Street, string City, string State, string Zip, string Email, bool PreferEmail)
+    : AddressBase(Street, City, State, Zip);
+
+public record ShippingAddress : AddressBase
+{
+    public string DeliveryInstructions { get; init; }
+    
+    public ShippingAddress(
+        string street, string city, string state, string zip, string deliveryInstructions)
+        : base(street, city, state, zip)
+        {
+            if (street.Contains("P.O. Box"))
+                throw new ArgumentException("P.O. Boxes aren't allowed");
+            DeliveryInstructions = deliveryInstructions;
+        }
+}
