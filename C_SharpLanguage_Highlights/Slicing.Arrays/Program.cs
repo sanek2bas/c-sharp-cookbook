bd68d922d1be2325453da06a7801192aad8f4278
TRBONET\a.gordeev <a.gordeev@trbonet.com>
2025-04-03 11:21:16 +0300

    Slicing.Arrays

---
+++ C_SharpLanguage_Highlights/Slicing.Arrays/Program.cs
@@ -0,0 +1,15 @@
+﻿using Slicing.Arrays;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        AddressService addressSvc = new();
+        foreach(var addresses in addressSvc.GetAddresses(3))
+        {
+            foreach(var address in addresses)
+                Console.WriteLine(address);
+            Console.WriteLine("\nNew Page\n");
+        }
+    }
+}
