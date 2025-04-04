8bd7db2c878b12b5734420e0ac45873ba7436fcf
TRBONET\a.gordeev <a.gordeev@trbonet.com>
2025-04-04 12:44:24 +0300

    Converting.Instances.Safely

---
+++ Matching_With_Patterns/Converting.Instances.Safely/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using Converting.Instances.Safely;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ProcessLegacyCode();
+        ProcessModernCode();
+    }
+
+    private static void ProcessModernCode()
+    {
+        List<IRoomSchedule> schedules = GetStrongTypedSchedules();
+        foreach (var schedule in schedules)
+        {
+            schedule.ScheduleRoom();
+            if (schedule is GoldSchedule gold)
+                Console.WriteLine($"Extra processing for {gold.GetType()}");
+}
+    }
+
+    private static List<IRoomSchedule> GetStrongTypedSchedules()
+    {
+        return new List<IRoomSchedule>
+        {
+            new BronzeSchedule(),
+            new SilverSchedule(),
+            new GoldSchedule()
+        };
+}
+
+    private static void ProcessLegacyCode()
+    {
+        ArrayList schedules = GetWeakTypedSchedules();
+        foreach (var schedule in schedules)
+        {
+            if (schedule is IRoomSchedule)
+            {
+                IRoomSchedule roomSchedule = (IRoomSchedule)schedule;
+                roomSchedule.ScheduleRoom();
+            }
+
+            IRoomSchedule asRoomSchedule = schedule as IRoomSchedule;
+            if (asRoomSchedule != null)
+                asRoomSchedule.ScheduleRoom();
+            
+            if (schedule is IRoomSchedule isRoomSchedule)
+                isRoomSchedule.ScheduleRoom();
+        }
+    }
+
+    private static ArrayList GetWeakTypedSchedules()
+    {
+        var list = new ArrayList();
+        list.Add(new BronzeSchedule());
+        list.Add(new SilverSchedule());
+        list.Add(new GoldSchedule());
+        return list;
+    }
+}
---
+++ Matching_With_Patterns/Converting.Instances.Safely/Room.cs
@@ -0,0 +1,24 @@
+namespace Converting.Instances.Safely;
+
+public interface IRoomSchedule
+{
+    void ScheduleRoom();
+}
+
+public class GoldSchedule : IRoomSchedule
+{
+    public void ScheduleRoom() =>
+        Console.WriteLine("Scheduling Gold Room");
+}
+
+public class SilverSchedule : IRoomSchedule
+{
+    public void ScheduleRoom() =>
+        Console.WriteLine("Scheduling Silver Room");
+}
+
+public class BronzeSchedule : IRoomSchedule
+{
+    public void ScheduleRoom() =>
+        Console.WriteLine("Scheduling Bronze Room");
+}
