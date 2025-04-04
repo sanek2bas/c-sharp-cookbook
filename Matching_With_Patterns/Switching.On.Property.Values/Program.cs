86dd35d34bd05d2faf324ed425d8488bdf382fc8
sanek2bas <sanek_2bas@mail.ru>
2025-04-04 21:35:19 +0300

    Switching.On.Property.Values

---
+++ Matching_With_Patterns/Switching.On.Property.Values/Program.cs
@@ -0,0 +1,67 @@
+﻿using Switching.On.Property.Values;
+
+internal class Program
+{
+    const int RoomNotAvailable = -1;
+
+    private static void Main(string[] args)
+    {
+        Console.Write(
+            "Choose (1) Bronze, (2) Silver, or (3) Gold: ");
+        string choice = Console.ReadLine();
+
+        Enum.TryParse(choice, out ScheduleType scheduleType);
+
+        int roomNumber = AssignRoom(scheduleType);
+        if (roomNumber == RoomNotAvailable)
+            Console.WriteLine("Room not available.");
+        else
+            Console.WriteLine($"The room number is {roomNumber}.");
+    }
+
+    private static int AssignRoom(ScheduleType scheduleType)
+    {
+        foreach (var room in GetRooms())
+        {
+            ScheduleType roomType = room switch
+            {
+                { BedSize: "King", RoomType: "Suite" }
+                    => ScheduleType.Gold,
+                { BedSize: "King", RoomType: "Regular" }
+                    => ScheduleType.Silver,
+                { BedSize: "Queen", RoomType: "Regular" }
+                    => ScheduleType.Bronze,
+                _ => ScheduleType.Bronze
+            };
+
+            if (roomType == scheduleType)
+                return room.Number;
+        }
+        return RoomNotAvailable;
+    }
+
+    private static List<Room> GetRooms()
+    {
+        return new List<Room>
+        {
+            new Room
+            {
+                Number = 333,
+                BedSize = "King",
+                RoomType = "Suite"
+            },
+            new Room
+            {
+                Number = 222,
+                BedSize = "King",
+                RoomType = "Regular"
+            },
+            new Room
+            {
+                Number = 111,
+                BedSize = "Queen",
+                RoomType = "Regular"
+            },
+        };
+    }
+}
