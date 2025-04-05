dbc948d59f4ac0b0d12735805fa0d8c2c3e86570
sanek2bas <sanek_2bas@mail.ru>
2025-04-05 21:03:25 +0300

    Switching.On.Value.Ranges

---
+++ Matching_With_Patterns/Switching.On.Value.Ranges/Program.cs
@@ -0,0 +1,29 @@
+﻿using Switching.On.Value.Ranges;
+
+internal class Program
+{
+    const int SilverPoints = 5000;
+    const int GoldPoints = 20000;
+
+    private static void Main(string[] args)
+    {
+        Console.Write("How many points? ");
+        string response = Console.ReadLine();
+        if (!int.TryParse(response, out int points))
+        {
+            Console.WriteLine($"'{response}' is invalid!");
+            return;
+        }
+        IRoomSchedule schedule = GetSchedule(points);
+        schedule.ScheduleRoom();
+    }
+
+    
+    static IRoomSchedule GetSchedule(int points) =>
+        points switch
+        {
+            >= GoldPoints => new GoldSchedule(),
+            >= SilverPoints => new SilverSchedule(),
+            < SilverPoints => new BronzeSchedule()
+        };
+}
