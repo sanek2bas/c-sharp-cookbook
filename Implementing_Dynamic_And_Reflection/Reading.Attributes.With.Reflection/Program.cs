5b7f65c1ab9544f6af8e256aaa47d673fdd42417
sanek2bas <sanek_2bas@mail.ru>
2025-04-07 00:42:11 +0300

    Reading.Attributes.With.Reflection

---
+++ Implementing_Dynamic_And_Reflection/Reading.Attributes.With.Reflection/Program.cs
@@ -0,0 +1,35 @@
+﻿using Reading.Attributes.With.Reflection;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var inventory = new List<object>
+        {
+            new InventoryItem
+            {
+                PartNumber = "1",
+                Description = "Part #1",
+                Count = 3,
+                ItemPrice = 5.26m
+            },
+            new InventoryItem
+            {
+                PartNumber = "2",
+                Description = "Part #2",
+                Count = 1,
+                ItemPrice = 7.95m
+            },
+            new InventoryItem
+            {
+                PartNumber = "3",
+                Description = "Part #3",
+                Count = 2,
+                ItemPrice = 23.13m
+            },
+        };
+        
+        string report = new Report().Generate(inventory);
+        Console.WriteLine(report);
+    }
+}
---
+++ Implementing_Dynamic_And_Reflection/Reading.Attributes.With.Reflection/Report.cs
@@ -0,0 +1,48 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace Reading.Attributes.With.Reflection
+{
+    public class Report
+    {
+        const string ColumnSeparator = " | ";
+
+        public string Generate(List<object> items)
+        {
+            _ = items ?? 
+                throw new ArgumentNullException($"{nameof(items)} is required");
+            MemberInfo[] members =
+                items.First().GetType().GetMembers();
+            var report = new StringBuilder("# Report\n\n");
+            report.Append(GetHeaders(members));
+            return report.ToString();
+        }
+
+        private StringBuilder GetHeaders(MemberInfo[] members)
+        {
+            var columnNames = new List<string>();
+            var underscores = new List<string>();
+
+            foreach (var member in members)
+            {
+                var attribute =
+                member.GetCustomAttribute<ColumnAttribute>();
+                if (attribute != null)
+                {
+                    string columnTitle = attribute.Name;
+                    string dashes = "".PadLeft(columnTitle.Length, '-');
+                    columnNames.Add(columnTitle);
+                    underscores.Add(dashes);
+                }
+            }
+
+            var header = new StringBuilder();
+            header.AppendJoin(ColumnSeparator, columnNames);
+            header.Append("\n");
+            header.AppendJoin(ColumnSeparator, underscores);
+            header.Append("\n");
+
+            return header;
+        }
+    }
+}
