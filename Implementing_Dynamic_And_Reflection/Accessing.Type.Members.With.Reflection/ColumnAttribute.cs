08b0c681fcdcbd548eb5532a7d90e934a16e3765
sanek2bas <sanek_2bas@mail.ru>
2025-04-07 12:42:45 +0300

    Accessing.Type.Members.With.Reflection

---
+++ Implementing_Dynamic_And_Reflection/Accessing.Type.Members.With.Reflection/ColumnAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace Accessing.Type.Members.With.Reflection
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Method, AllowMultiple = false)]
+    public class ColumnAttribute : Attribute
+    {
+        public ColumnAttribute(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+
+        public string Format { get; set; }
+    }
+}
