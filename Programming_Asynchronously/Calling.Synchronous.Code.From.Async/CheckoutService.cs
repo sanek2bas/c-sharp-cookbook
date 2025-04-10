42799016837c7a8c8dd1decf6639aa22be7947c1
sanek2bas <sanek_2bas@mail.ru>
2025-04-10 18:55:58 +0300

    Calling.Synchronous.Code.From.Async

---
+++ Programming_Asynchronously/Calling.Synchronous.Code.From.Async/CheckoutService.cs
@@ -0,0 +1,37 @@
+﻿namespace Calling.Synchronous.Code.From.Async
+{
+    public class CheckoutService
+    {
+        public async Task<string> StartAsync()
+        {
+            await ValidateAddressAsync().ConfigureAwait(false);
+            await ValidateCreditAsync().ConfigureAwait(false);
+            await GetShoppingCartAsync().ConfigureAwait(false);
+            await FinalizeCheckoutAsync().ConfigureAwait(false);
+            return "Checkout Complete";
+        }
+        
+        private async Task<bool> ValidateAddressAsync()
+        {
+            bool result = true;
+            return await Task.FromResult(result);
+        }
+
+        private async Task<bool> ValidateCreditAsync()
+        {
+            bool result = true;
+            return await Task.FromResult(result);
+        }
+
+        private async Task<bool> GetShoppingCartAsync()
+        {
+            bool result = true;
+            return await Task.FromResult(result);
+        }
+
+        private async Task FinalizeCheckoutAsync()
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ Programming_Asynchronously/Calling.Synchronous.Code.From.Async/Program.cs
@@ -0,0 +1,11 @@
+﻿using Calling.Synchronous.Code.From.Async;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var checkoutSvc = new CheckoutService();
+        string result = await checkoutSvc.StartAsync();
+        Console.WriteLine($"Result: {result}");
+    }
+}
