5650d2e5322ff68d2902507c2bfffdfd5f4f8649
sanek2bas <sanek_2bas@mail.ru>
2025-04-10 12:20:51 +0300

    Reducing.Memory.Allocations.For.Async

---
+++ Programming_Asynchronously/Reducing.Memory.Allocations.For.Async/CheckoutService.cs
@@ -0,0 +1,34 @@
+﻿namespace Reducing.Memory.Allocations.For.Async
+{
+    public class CheckoutService
+    {
+        public async ValueTask<string> StartAsync()
+        {
+            await ValidateAddressAsync();
+            await ValidateCreditAsync();
+            await GetShoppingCartAsync();
+            await FinalizeCheckoutAsync();
+            return "Checkout Complete";
+        }
+
+        private async ValueTask ValidateAddressAsync()
+        {
+            // perform address validation
+        }
+
+        private async ValueTask ValidateCreditAsync()
+        {
+            // ensure credit is good
+        }
+        
+        private async ValueTask GetShoppingCartAsync()
+        {
+            // get contents of shopping cart
+        }
+        
+        private async ValueTask FinalizeCheckoutAsync()
+        {
+            // complete checkout transaction
+        }
+    }
+}
---
+++ Programming_Asynchronously/Reducing.Memory.Allocations.For.Async/Program.cs
@@ -0,0 +1,11 @@
+﻿using Reducing.Memory.Allocations.For.Async;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var checkoutSvc = new CheckoutService();
+        string result = await checkoutSvc.StartAsync();
+        Console.WriteLine($"Result: {result}");
+    }
+}
