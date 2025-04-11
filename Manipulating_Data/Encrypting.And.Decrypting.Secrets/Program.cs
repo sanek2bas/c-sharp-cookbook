d2a2537b7e182d8eb07c3a309ca17fffff0b606f
sanek2bas <sanek_2bas@mail.ru>
2025-04-11 23:07:59 +0300

    Encrypting.And.Decrypting.Secrets

---
+++ Manipulating_Data/Encrypting.And.Decrypting.Secrets/Program.cs
@@ -0,0 +1,27 @@
+﻿using Encrypting.And.Decrypting.Secrets;
+using System.Security.Cryptography;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var crypto = new Crypto();
+        Console.Write("Please enter text to encrypt: ");
+        string userPlainText = Console.ReadLine();
+        byte[] key = GenerateKey();
+        byte[] cypherBytes = crypto.Encrypt(userPlainText, key);
+        string cypherText = Convert.ToBase64String(cypherBytes);
+        Console.WriteLine($"Cypher Text: {cypherText}");
+        string decryptedPlainText = crypto.Decrypt(cypherBytes, key);
+        Console.WriteLine($"Plain Text: {decryptedPlainText}");
+    }
+
+    private static byte[] GenerateKey()
+    {
+        const int KeyLength = 32;
+        byte[] key = new byte[KeyLength];
+        var rngRand = RandomNumberGenerator.Create();
+        rngRand.GetBytes(key);
+        return key;
+    }
+}
