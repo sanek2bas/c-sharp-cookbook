bc967f44891d34378b4695d2ca65ac694da38291
sanek2bas <sanek_2bas@mail.ru>
2025-04-11 23:53:54 +0300

    Hiding.Development.Secrets

---
+++ Manipulating_Data/Hiding.Development.Secrets/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var config = new ConfigurationBuilder()
+            .AddUserSecrets<Program>()
+            .Build();
+        string key = "CSharpCookbook:ApiKey";
+        Console.WriteLine($"{key}: {config[key]}");
+    }
+}
