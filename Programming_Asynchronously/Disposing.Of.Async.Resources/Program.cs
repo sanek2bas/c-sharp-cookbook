5a6bb0de63e05d6720009428ffc2bfae28c33d08
TRBONET\a.gordeev <a.gordeev@trbonet.com>
2025-04-11 13:10:42 +0300

    refactoring

--- Programming_Asynchronously/Disposing.Of.Async.Resources/Program.cs
+++ Programming_Asynchronously/Disposing.Of.Async.Resources/Program.cs
@@ -1,7 +1,40 @@
 ﻿internal class Program
 {
-    private static void Main(string[] args)
+    private static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var fileName = "DeploymentReport.txt";
+        var baseDirectory = GetDataFolder(AppDomain.CurrentDomain.BaseDirectory, 
+                                          AppDomain.CurrentDomain.FriendlyName);
+        var filepath = $"{baseDirectory}\\{fileName}";
+
+        await using var checkoutStrm = new CheckoutStream(filepath);
+        var checkoutSvc = new CheckoutService();
+        IProgress<CheckoutRequestProgress> progress =
+            new Progress<CheckoutRequestProgress>(p =>
+            {
+                Console.WriteLine(
+                    $"\n" +
+                    $"Total: {p.Total}, " +
+                    $"{p.Message}" +
+                    $"\n");
+            });
+            
+        int count = 1;
+        await foreach (var request in 
+            checkoutStrm.GetRequestsAsync(progress))
+        {
+            string result = await checkoutSvc.StartAsync(request);
+            Console.WriteLine($"Result: {result}");
+            if (count++ >= 10)
+                break;
+        }
+    }
+
+    private static string GetDataFolder(string startDirectory, string targetFolder)
+    {
+        var directory = Directory.GetParent(startDirectory);
+        while (directory.Name != targetFolder)
+               directory = directory.Parent;
+        return directory.FullName;
     }
 }
