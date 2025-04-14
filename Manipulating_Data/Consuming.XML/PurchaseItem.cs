c2f8970d888bc1d8b354e3f531d7610b3b55b636
sanek2bas <sanek_2bas@mail.ru>
2025-04-14 14:25:32 +0300

    Consuming.XML

---
+++ Manipulating_Data/Consuming.XML/PurchaseItem.cs
@@ -0,0 +1,10 @@
+﻿namespace Consuming.XML
+{
+    public class PurchaseItem
+    {
+        public string SerialNumber { get; set; }
+        public string Description { get; set; }
+        public float Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
