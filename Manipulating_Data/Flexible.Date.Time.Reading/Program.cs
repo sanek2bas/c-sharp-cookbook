d17375120a2ff9b8cb551d517b3b4b7117959a9c
sanek2bas <sanek_2bas@mail.ru>
2025-04-15 23:40:22 +0300

    Flexible.Date.Time.Reading

---
+++ Manipulating_Data/Flexible.Date.Time.Reading/Program.cs
@@ -0,0 +1,25 @@
+﻿using Flexible.Date.Time.Reading;
+using System.Text.Json;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        const string TweetID = "1305895383260782593";
+        const string CreatedDate = "created_at";
+        string tweetJson = GetTweet(TweetID);
+        JsonElement tweetElem = JsonDocument.Parse(tweetJson).RootElement;
+        DateTime created = tweetElem.GetDate(CreatedDate);
+        Console.WriteLine($"Created Date: {created}");
+    }
+
+    private static string GetTweet(string tweetID)
+    {
+        return @"{
+            ""text"": ""Thanks @github for approving sponsorship for LINQ to Twitter: https://t.co/jWeDEN07HN"",
+            ""id"": ""1305895383260782593"",
+            ""author_id"": ""15411837"",
+            ""created_at"": ""2020-09-15T15:44:56.000Z""
+        }";
+    }
+}
